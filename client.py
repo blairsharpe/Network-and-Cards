07837bc8888b5a41ea9b26611e8a5fb2e0c9ada6
Blair Sharpe <blair.a.sharpe@gmail.com>
2018-01-28 16:10:15 -0500

    Addition of threading

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ def client_broadcast_receive(IP, PORT, BUFFER_SIZE):
     data = s.recvfrom(BUFFER_SIZE)
     message, socket_info = data
     print(message.decode("utf-8"))
-    print("IP: {}, PORT: {}".format(socket_info[0], socket_info[1]))
+    print("(server -> client) IP: {}, Port: {}".format(socket_info[0], socket_info[1]))
 
     return True
 
@@ -20,4 +20,15 @@ if __name__ == "__main__":
     TCP_PORT = 5005
     BUFFER_SIZE = 1024
 
-    client_broadcast_receive(TCP_IP, TCP_PORT, BUFFER_SIZE)
+    if client_broadcast_receive(TCP_IP, TCP_PORT, BUFFER_SIZE) is True:
+
+        host = socket.gethostname()
+        port = 2005
+        BUFFER_SIZE = 2000
+        MESSAGE = "username: BlairSharpe"
+
+        tcpClientA = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcpClientA.connect((host, port))
+        tcpClientA.send(MESSAGE.encode("utf-8"))
+
+        tcpClientA.close()
--- server.py
+++ server.py
@@ -1,9 +1,23 @@
 import socket
+from threading import Thread
+
+
+class ClientThread(Thread):
+
+    def __init__(self, ip, port):
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        print("(client - > server) + New thread for: IP {}, Port: {}".format(ip, port))
+
+    def run(self):
+        data = conn.recv(2048)
+        print("(client -> server) Server received data: {}".format(data.decode("utf-8")))
 
 
 def server_broadcast_send():
 
-    MESSAGE = "Black Jack Server Running"
+    MESSAGE = "(server - > client) Black Jack server up"
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -14,4 +28,26 @@ def server_broadcast_send():
 
 if __name__ == "__main__":
 
+    TCP_IP = '10.0.1.173'
+    TCP_PORT = 2005
+    BUFFER_SIZE = 20  # Usually 1024, but we need quick response
+
     server_broadcast_send()
+
+    tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    tcpServer.bind((TCP_IP, TCP_PORT))
+
+    threads = []
+
+    server_broadcast_send()
+
+    tcpServer.listen(4)
+    print("Waiting for clients...")
+    (conn, (ip, port)) = tcpServer.accept()
+    newthread = ClientThread(ip, port)
+    newthread.start()
+    threads.append(newthread)
+
+    for t in threads:
+        t.join()

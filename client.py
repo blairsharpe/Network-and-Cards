3e392c23b326976292224dd944ba0b34b4b2184c
Blair Sharpe <blair.a.sharpe@gmail.com>
2018-01-28 00:01:11 -0500

    Better User Output

--- client.py
+++ client.py
@@ -1,14 +1,23 @@
 import socket
 
 
-def client_broadcast_receive():
+def client_broadcast_receive(IP, PORT, BUFFER_SIZE):
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(('10.0.1.173', 5005))
-    m = s.recvfrom(1024)
-    print(print(m))
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((IP, PORT))
+    data = s.recvfrom(BUFFER_SIZE)
+    message, socket_info = data
+    print(message.decode("utf-8"))
+    print("IP: {}, PORT: {}".format(socket_info[0], socket_info[1]))
+
+    return True
 
 
 if __name__ == "__main__":
 
-    client_broadcast_receive()
+    TCP_IP = '10.0.1.173'
+    TCP_PORT = 5005
+    BUFFER_SIZE = 1024
+
+    client_broadcast_receive(TCP_IP, TCP_PORT, BUFFER_SIZE)
--- server.py
+++ server.py
@@ -2,10 +2,14 @@ import socket
 
 
 def server_broadcast_send():
-    MESSAGE = "This is a test"
+
+    MESSAGE = "Black Jack Server Running"
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     s.sendto(MESSAGE.encode('utf-8'), ('10.0.1.255', 5005))
+
+    return True
 
 
 if __name__ == "__main__":
